7e845bd6b6736ac4de46c1d26e712f71e0800b3f
ranaazeem786 <ranaazeem786@hotmail.com>
2020-06-26 21:38:24 +0500

    Create qualification.js

---
+++ src/qualification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Myqualification(props) {
+   return (
+       <div>
+           <hi>I have qualified {props.qualif}.</hi>
+       </div>
+   )
+}
+
+export default Myqualification;
