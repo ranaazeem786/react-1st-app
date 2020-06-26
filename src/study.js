aa134ac7d93bc0c340d3e115c1921b9580769f51
ranaazeem786 <ranaazeem786@hotmail.com>
2020-06-26 21:38:20 +0500

    Create study.js

---
+++ src/study.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Study(props) {
+   return (
+       <div>
+           <hi>I am a student of {props.student} at {props.inst}.</hi>
+       </div>
+   )
+}
+
+export default Study;
