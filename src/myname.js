3fcc28a97facda6b02bb7d5617fd04b3e91ac721
ranaazeem786 <ranaazeem786@hotmail.com>
2020-06-26 21:38:29 +0500

    Create myname.js

---
+++ src/myname.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Myname(props) {
+   return (
+       <div>
+           <hi>My name is {props.myname}.</hi>
+       </div>
+   )
+}
+
+export default Myname;
