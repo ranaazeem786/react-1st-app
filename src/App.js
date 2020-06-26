82f92a4ea27d10db3148fa53c786a12d364ef5d7
ranaazeem786 <ranaazeem786@hotmail.com>
2020-06-26 21:38:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Myname from './myname.js';
+import Myqualification from './qualification.js';
+import Study from './study.js';
+import Contact from './contact.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World
-        </p>
-      </header>
+   return (
+    <div>
+      <Myname myname="Azeem Ahmad" />
+      <Myqualification qualif="MBA and LLB "/>
+      <Study inst="PIAIC" student= "CNC and React"></Study>
+      <Contact mycontact="+92-300-8665352" myemail="ranaazeem786@hotmail.com" />
     </div>
-  );
+   )
+   
 }
 
 export default App;
