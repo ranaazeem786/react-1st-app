a675b62408767d2b5e5bd305d12b0520e3df9193
ranaazeem786 <ranaazeem786@hotmail.com>
2020-06-26 21:43:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Myqualification qualif="MBA and LLB "/>
       <Study inst="PIAIC" student= "CNC and React"></Study>
       <Contact mycontact="+92-300-8665352" myemail="ranaazeem786@hotmail.com" />
+      <p>by profession i am a Corporate Banker whereas i am trying to generate a new income streem thats why i am also trying to develop new skills.</p>
     </div>
    )
    
